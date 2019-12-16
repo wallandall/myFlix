e0a8d7e04e9ec025b03413d3024e81f76986af87
wallandall <wallandall@gmail.com>
2019-12-16 21:10:06 +0100

    Test DB Error

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -2,24 +2,29 @@ const mongoose = require('mongoose');
 
 const AppError = require('./appError');
 
-const getBDStatus = () => {
-  const dbStatus = mongoose.connection.readyState;
-  console.log(dbStatus);
-
-  if (dbStatus !== 1) {
-    return new AppError('DB Connection Error', 500);
-  }
-};
-
 module.exports = fn => {
-  return getBDStatus();
-  () => {
-    return (req, res, next) => {
-      fn(req, res, next).catch(next);
-    };
+  return (req, res, next) => {
+    fn(req, res, next).catch(next);
   };
 };
-//
+// const getBDStatus = () => {
+//   const dbStatus = mongoose.connection.readyState;
+//   console.log(dbStatus);
+
+//   if (dbStatus !== 1) {
+//     return new AppError('DB Connection Error', 500);
+//   }
+// };
+
+// module.exports = fn => {
+//   return getBDStatus();
+//   () => {
+//     return (req, res, next) => {
+//       fn(req, res, next).catch(next);
+//     };
+//   };
+// };
+// //
 
 /*readyState
 0: disconnected
